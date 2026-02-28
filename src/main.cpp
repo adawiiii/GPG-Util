6144f0e5544d0efe2bba41bcfe629703ff356521
Didi Sinai <dsadaw14@gmail.com>
2026-03-01 00:03:27 +0200

    feat: disabled cursor visibility & changed waiting symbols

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 #include<future>
 #include <thread>
 
+constexpr int wait_time_ms = 250;
+
 enum CommandType {
     KILL,
     LAUNCH
 };
+
+void set_cursor_visibility(bool visible) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info{};
+    GetConsoleCursorInfo(h, &info);
+    info.bVisible = visible;
+    SetConsoleCursorInfo(h, &info);
+    return;
+}
 
 DWORD run_command(std::string command) {
     STARTUPINFOA si = {sizeof(si)};
@@ -43,14 +54,17 @@ std::future<DWORD> async_powershell_command(std::string command) {
 void wait_loop(CommandType type, int attempt, std::future<DWORD>& promise) {
     switch (type) {
         case KILL: {
+            const char frames[] = {'.', ':', '*'};
+            size_t i = 0;
             while (promise.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
-                std::cout << "Killing GPG (" << attempt << ").  \r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
-                std::cout << "Killing GPG (" << attempt << ").. \r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
-                std::cout << "Killing GPG (" << attempt << ")...\r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+                std::cout << '\r'
+                          << std::left << std::setw(32)
+                          << ("Killing GPG (" + std::to_string(attempt) + ") " + frames[i % 3])
+                          << std::flush;
+                ++i;
+                std::this_thread::sleep_for(std::chrono::milliseconds(wait_time_ms));
             }
+            std::cout << '\r' << std::string(32, ' ') << '\r';
             DWORD k_ret = promise.get();
             if (k_ret == 0) {
                 std::cout << "Killed GPG (" << attempt << ")    " << std::endl;
@@ -60,14 +74,17 @@ void wait_loop(CommandType type, int attempt, std::future<DWORD>& promise) {
             break;
         }
         case LAUNCH: {
+            const char frames[] = {'.', ':', '*'};
+            size_t i = 0;
             while (promise.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
-                std::cout << "Launching GPG (" << attempt << ").  \r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
-                std::cout << "Launching GPG (" << attempt << ").. \r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
-                std::cout << "Launching GPG (" << attempt << ")...\r";
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+                std::cout << '\r'
+                          << std::left << std::setw(32)
+                          << ("Launching GPG (" + std::to_string(attempt) + ") " + frames[i % 3])
+                          << std::flush;
+                ++i;
+                std::this_thread::sleep_for(std::chrono::milliseconds(wait_time_ms));
             }
+            std::cout << '\r' << std::string(32, ' ') << '\r';
             DWORD l_ret = promise.get();
             if (l_ret == 0) {
                 std::cout << "Launched GPG (" << attempt << ")    " << std::endl;
@@ -86,17 +103,19 @@ int main() {
     std::string kill_command = "powershell -NoProfile -Command \"gpgconf --kill gpg-agent\"";
     std::string launch_command = "powershell -NoProfile -Command \"gpgconf --launch gpg-agent\"";
 
+    set_cursor_visibility(false);
     std::future<DWORD> kill = async_powershell_command(kill_command);
     wait_loop(KILL, 1, kill);
     kill = async_powershell_command(kill_command);
     wait_loop(KILL, 2, kill);
     kill = async_powershell_command(kill_command);
     wait_loop(KILL, 3, kill);
-
+    
     std::future<DWORD> launch = async_powershell_command(launch_command);    
     wait_loop(LAUNCH, 1, launch);
     launch = async_powershell_command(launch_command);    
     wait_loop(LAUNCH, 2, launch);
+    set_cursor_visibility(true);
 
     std::cout << "GPG Cleared" << std::endl;
     return 0;
